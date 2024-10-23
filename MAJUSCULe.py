d81c62540dc763f75e1f9eed8840587d178bb214
PaulAdr173 <pauadrian102@gmail.com>
2024-10-23 19:59:05 +0300

    Add files via upload

---
+++ MAJUSCULe.py
@@ -0,0 +1,10 @@
+import re
+
+txt = input("Introduceti textul aici:")
+a =txt.replace("a","A")
+b = a.replace("e", "E")
+c = b.replace("i", "I")
+d = c.replace("o", "O")
+e = d.replace("u", "U")
+print(e)
+
